1d3deb5278b080ca452265a4fe1ee7ab793c4caf
Wilhelm B <buch.willi@googlemail.com>
2017-03-15 16:36:38 +0100

    it works praise the lord

--- bloodshard.c
+++ bloodshard.c
@@ -3,9 +3,10 @@
 int main(int argc, char const *argv[]){
 	int fd = open("testfile", O_RDWR); /*worked just fine*/
 	int filesize = getFilesizeFD(fd); /*worked just fine*/
-	char * filecontent;
+	char filecontent[]= "AAAAAAAAAABBBBBBBBBBCCCCCCCCCCDDDDDDDDDDE";
 	read(fd, &filecontent, filesize);
 	close(fd);
-	syscall1(60,(uintptr)0);
+	puts(filecontent);
+
 	return 0;
 }
